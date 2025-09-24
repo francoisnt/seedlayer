dbd7641a6cba7f5a98644e1147799efb4c42b01e
François Naggar-Tremblay <francois.n.tremblay@gmail.com>
2025-09-23 18:28:03 -0600

    started integration test

---
+++ tests/integration/sqlite/chinook-database.py
@@ -0,0 +1,1 @@
+# TODO: implement integration test for seedlayer that creates an inmemory database, runs the chinook-database.sql sql script to create the tables and then runs seedlayer apropriately to test it.
